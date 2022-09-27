ee906f3247f5819c2bd207d5b5c8f66ed9b36c43
vivian <viviantitus13@yahoo.in>
2022-09-27 19:59:18 +0530

    profiler; cosine_similarity

--- src/main.py
+++ src/main.py
@@ -1,5 +1,22 @@
+import torch
+from profiler import profileit
 
+def get_rand_query(dimension: int = 512):
+    return torch.rand(size=(dimension,))
 
+def dataset_generator(dataset_size, dimension: int = 512):
+    for i in range(dataset_size):
+        yield torch.rand(size=(dimension,))
 
-def hello_world():
-    return "hello"
+def cosine_similarity(x, y):
+    dot_product = torch.dot(x, y)
+    norms = torch.norm(x) * torch.norm(y)
+    return dot_product/ (norms + 1e-6)
+
+@profileit
+def main():
+    query = get_rand_query()
+    for sample in dataset_generator(1000):
+        print(cosine_similarity(query, sample))
+
+main()
