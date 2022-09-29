ecd6cbd4b62dc5a6db58bf15f59f27e5e6f67dc9
vivian <viviantitus13@yahoo.in>
2022-09-29 19:58:43 +0530

    profile_with_args

--- src/main.py
+++ src/main.py
@@ -1,26 +1,30 @@
 import torch
-from profiler import profileit
+from profiler import profile_with_args
 
 
 def get_rand_query(dimension: int = 512):
     return torch.rand(size=(1, dimension))
 
-def dataset_generator(dataset_size, dimension: int = 512):
-    default_size = 1000000
-    for i in range(int(dataset_size/default_size)):
-        yield torch.rand(size=(dimension, default_size))
+def slice_func(big_number, slice_number):
+    gen_list = [slice_number for i in range(int(big_number/slice_number))]
+    if big_number % slice_number != 0:
+        gen_list.append(big_number % slice_number)
+    return gen_list
 
+def dataset_generator(dataset_size, dimension: int = 512):
+    for slice_size in slice_func(dataset_size, 1000000):
+        yield torch.rand(size=(dimension, slice_size))
+
+@profile_with_args(enabled=False)
 def similiarity(q, dataset):
     num = torch.mm(q, dataset)
     den = torch.norm(q) * torch.norm(dataset, dim=0)
     return torch.flatten(num)/den
 
-@profileit
 def main(dataset_size):
     q = get_rand_query()
     for dataset in dataset_generator(dataset_size):
         similiarity(q, dataset)
 
 
-     
-main(dataset_size = 10000000)
+main(dataset_size = 1000000)
--- src/profiler.py
+++ src/profiler.py
@@ -1,17 +1,23 @@
 import cProfile
 
-def profileit(func):
-    """
-	Decorator (function wrapper) that profiles a single function
-	@profileit()
-	def func1(...)
-            # do something
-	    pass
-    """
-    def wrapper(*args, **kwargs):
-        prof = cProfile.Profile()
-        retval = prof.runcall(func, *args, **kwargs)
-        prof.print_stats()
-        return retval
+def profile_with_args(enabled):
 
-    return wrapper
+    def profileit(func):
+        """
+        Decorator (function wrapper) that profiles a single function
+        @profileit()
+        def func1(...)
+                # do something
+            pass
+        """
+        def wrapper(*args, **kwargs):
+            if enabled:
+                prof = cProfile.Profile()
+                retval = prof.runcall(func, *args, **kwargs)
+                prof.print_stats()
+                return retval
+            else:
+                return func(*args, **kwargs)
+
+        return wrapper
+    return profileit
