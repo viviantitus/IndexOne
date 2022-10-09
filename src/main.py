bd4f5f3a1fdfb429c37cae69437e4dc6bcb08041
vivian <viviantitus13@yahoo.in>
2022-10-09 22:08:27 +0530

    pq_adc

--- src/main.py
+++ src/main.py
@@ -14,13 +14,13 @@ args = parser.parse_args()
 @profileit(enabled=args.profile)
 def main():
     dataset = torch.cat([kmeans.get_dataset(100, 512) * 2, kmeans.get_dataset(100, 512)])
-    codes, encoded_dataset = pq.train(dataset, num_chunks=32, num_centroids_per_chunk=8, chunk_dim=16)
+    codes, _ = pq.train(dataset, num_chunks=32, num_centroids_per_chunk=8, chunk_dim=16)
     encoded_dataset = pq.encode(dataset, codes, 32, 8)
-    distance_map = pq.compute_distance_map_for_centroids(codes, 32, 8)
 
 
-    query = torch.stack([dataset[0, :], dataset[10, :]])
-    encoded_queries = pq.encode(query, codes, 32, 8)
-    pq.get_scores(encoded_dataset, encoded_queries, distance_map, 32)
+    query = torch.stack([dataset[45, :], dataset[111, :]])
+    distances = pq.adc_pq(query, encoded_dataset, codes, 32, 16)
+    print(torch.topk(distances, k=5, largest=False, dim=1))
+
 
 main()
--- src/pq.py
+++ src/pq.py
@@ -41,4 +41,15 @@ def get_scores(encoded_dataset, encoded_queries, distance_map, num_chunks):
                 scores[i, j] = torch.sum(distance_map[:, encoded_dataset[j, chunk_indx_], encoded_queries[i, chunk_indx_]])
     return scores
 
+def adc_pq(query, encoded_dataset, codes, num_chunks, chunk_dim):
+    encoded_dataset_len = encoded_dataset.size()[0]
+    query_len = query.size()[0]
+    distances = torch.zeros((query_len, encoded_dataset_len, num_chunks))
+
+    for k, sub_vec in enumerate(split_to_subvecs(query, num_chunks=num_chunks)):
+        for i in range(encoded_dataset_len):
+            distances[:, i, k] = compute_distance(codes[k, encoded_dataset[i, k], :].unsqueeze(0), sub_vec).squeeze(0)
+
+    return torch.sum(distances, dim=2)
+
 
